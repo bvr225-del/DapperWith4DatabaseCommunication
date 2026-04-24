6cd97b54f787164a75832325e11f32ad406379ee
venkat <dell@DESKTOP-S8CP3CH>
2026-04-24 10:49:25 +0530

    userstory-123480-createcrudoperations for orders using dapper

--- DapperWith4DatabaseCommunication/Utils/StoredprocedureParameters.cs
+++ DapperWith4DatabaseCommunication/Utils/StoredprocedureParameters.cs
@@ -13,6 +13,8 @@
         public static string OrderId = "@orderid";
         public static string OrderName = "@ordername";
         public static string OrderLocation = "@orderlocation";
+        public static string OrderInsertedvariable = "@insertedvalue";
+
         #endregion
 
         #region Employee Parameters
