bac0f8fb64c4fd9b6f33c2a3eb20a8ab6fcb2c11
venkat <dell@DESKTOP-S8CP3CH>
2026-05-09 17:21:21 +0530

    some codwe updated

--- DapperWith4DatabaseCommunication/Program.cs
+++ DapperWith4DatabaseCommunication/Program.cs
@@ -85,22 +85,56 @@ builder.Services.AddCors(options =>
 #endregion
 //section2:app is the inbuilt request pipeline,heare we need to register our middlewares to application pipeline.
 //===================================================================================================================
-
-
-var app = builder.Build();
+#region middlewaresConfigurationSection
+var app = builder.Build();//app is requtest pipeline,it is created at runtime.
+                          //all middleware we need to configure/register /adding to this request pipeline
+                          //whenever if you add any middleware to request pipeleine order wise it will exceute.
+                          //for inline middlewares all logic written in the app.use() method and for custom middlewares we need to create a class and write the logic in that class and then we need to register that custom middleware in the program.cs file using the app.UseMiddleware method.
+                          // middleware naming convention starts with use keyword.                         
+                          //we must register the middle wares to request pipleline.based on order you register.same order it will excute.
+                          //**below code is for inline middlewares,here we are writing the logic of the middleware inside the app.use() method.**                         
+                          //******inline middleware logic starts here********
+                          //For inline middleware we are writing the logic of the middleware inside the app.use() method. we can write multiple inline middlewares in the program.cs file by using multiple app.use() methods. and also we need to call the next.invoke() method in the app.use() method to call the next middleware in the pipeline. if you are not calling next.invoke() method in the app.use() method, it will not call the next middleware in the pipeline and it will stop the execution of the middleware.
+                          //app.Use(async (context, next) => {//app.Use for Inline middlewares
+                          //    await context.Response.WriteAsync("Hello I am From use1");
+                          //    await next.Invoke();//if you are not calling next.invoke() method in the app.use() method, it will not call the next middleware in the pipeline and it will stop the execution of the middleware.
+                          //});
+                          //app.Use(async (context, next) => {//app.Use for Inline middlewares
+                          //    await context.Response.WriteAsync("Hello I am From use2");
+                          //    await next.Invoke();
+                          //});
+                          //******inline middleware logic ended here********
+/*
+ * syntax:Adds a custom  middleware type to the application request Pipeline like below syntax .
+ * =============================================================================================
+  app.UseMiddleWare<CustomMiddlewareClassName>();we must register like this way.
+//<>   we called its as placeholder  .or AngleBracktes
+//In that PlaceHolder (<Custommiddlware classname>)Write here
+//app.UseMiddleware<RequestLoggingMiddleware>();//Registering the  Custom Middleware to appliction pipeline like this way.
+// Register the middlewares in  HTTP request pipeline.
+*/
 //custom middlewares we need to register in the program.cs file of the web api project using the UseMiddleware method   app object. The UseMiddleware method is used to add custom middleware components to the application's request processing pipeline. By adding the GlobalErrorHandlerMiddleware, you ensure that any unhandled exceptions that occur during the processing of HTTP requests will be caught and handled by this middleware, allowing you to return a standardized error response to the client and log the error details as needed.
-app.UseMiddleware<GlobalErrorHandlerMiddleware>();//This line of code is used to add the GlobalErrorHandlerMiddleware to the application's request processing pipeline. The UseMiddleware method is an extension method that allows you to add custom middleware components to the pipeline. By adding the GlobalErrorHandlerMiddleware, you ensure that any unhandled exceptions that occur during the processing of HTTP requests will be caught and handled by this middleware, allowing you to return a standardized error response to the client and log the error details as needed.
-
-
+app.UseMiddleware<GlobalErrorHandlerMiddleware>();//Registering the  Custom Middleware to appliction pipeline like this way.
+app.UseMiddleware<RequestLoggingMiddleware>();//Registering the  Custom Middleware to appliction pipeline like this way.
+//This line of code is used to add the GlobalErrorHandlerMiddleware to the application's request processing pipeline. The UseMiddleware method is an extension method that allows you to add custom middleware components to the pipeline. By adding the GlobalErrorHandlerMiddleware, you ensure that any unhandled exceptions that occur during the processing of HTTP requests will be caught and handled by this middleware, allowing you to return a standardized error response to the client and log the error details as needed.
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger();//Predefined Middlewares,created by the swagger team to generate the swagger documentation for the api.
     app.UseSwaggerUI();
 }
-
-app.UseAuthorization();
+//UseCors is a predefined middleware,created by the Microsoft team to handle the cross-origin resource sharing in the api level.
+app.UseCors();//add the cors middleware to the application pipeline and specify the policy name that we defined in the AddCors method of the builder object in the dependency injection container section.
+//app.UseCors("bankPolicy");//this cors is for specific policy,if you want to enable the cors for specific policy then you need to specify the policy name in the app.useCors() method like this way.
+app.UseAuthorization();//Predefined Middlewares,created by the Microsoft team to handle the authorization in the api.
 
 app.MapControllers();
 
-app.Run();
+app.Run();//App.run() is a termainal middleware,it ends the application pipeline without calling the next middleware.always app.run() is last in program.cs file
+          //always it should be ending only.
+          //(Here We removed App.Run() method,without app.run() if you run the application it will throw error like "WebServer failed to listen on port 5296")
+          //After app.run () method if you write any code it will not exceute.because app.run() does not having next.due to that exceution is stopped in this  line.
+
+
+
+#endregion
